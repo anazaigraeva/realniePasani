edcdb82409c2e9b25d0bfd6e236b69ccd2a382ab
Nastyusha <tor4ebes@mail.ru>
2025-09-25 11:18:32 +0300

    404

---
+++ client/src/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>Ага! Попався!</h1>
+      <img
+        src="https://s1.ezgif.com/tmp/ezgif-171be38ea679d4.gif"
+        alt="Ага! Попався! GIF"
+      />
+    </div>
+  );
+}
