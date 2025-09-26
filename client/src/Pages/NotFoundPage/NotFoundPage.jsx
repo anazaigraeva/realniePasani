4b0b6b74459d060026e7d132a13ccefc3f5bc2b4
Nastyusha <tor4ebes@mail.ru>
2025-09-26 11:35:03 +0300

    i want to die please

--- client/src/Pages/NotFoundPage/NotFoundPage.jsx
+++ client/src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import './NotFoundPage.css';
 
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="notfound-container">
       <h1>Ага! Попався!</h1>
       <img
-        src="https://s1.ezgif.com/tmp/ezgif-171be38ea679d4.gif"
+        src="/public/PixVerse_V5_Image_Text_720P_plese_do_some_very.gif"
         alt="Ага! Попався! GIF"
+        className="notfound-gif"
       />
+      <button className="home-btn" onClick={() => navigate('/')}>
+        Вернуться на главную
+      </button>
     </div>
   );
 }
--- server/src/app.js
+++ server/src/app.js
@@ -6,9 +6,8 @@ const categoryRouter = require('./Routes/categoryRouter');
 const wordRouter = require('./Routes/wordRouter');
 const authRouter = require('./Routes/authRouter');
 const likesRouter = require('./Routes/likesRouter');
+const aiRouter = require('./Routes/chatRouter');
 
-const wordCategRouter = require('./Routes/wordCategRouter');
-const chatRouter = require('./Routes/chatRouter');
 const app = express()
 
 
@@ -22,6 +21,6 @@ app.use('/api/words', wordRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter);
 app.use('/api/likes', likesRouter);
-app.use('/api/chat', chatRouter);
+app.use('/api/ai', aiRouter );
 
 module.exports = app;
